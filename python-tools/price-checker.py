b95cbc876a888821d76b86c316ceeb35aed1a5e2
sombody <rayn.hasan786@gmail.com>
2025-06-29 22:14:22 +0200

    updated with ETH price

--- python-tools/price-checker.py
+++ python-tools/price-checker.py
@@ -1,6 +1,7 @@
 import requests
 
-response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
-btc_price = response.json()["bitcoin"]["usd"]
+response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd")
+data = response.json()
 
-print("Bitcoin Price (USD):", btc_price)
+print("Bitcoin Price (USD):", data["bitcoin"]["usd"])
+print("Ethereum Price (USD):", data["ethereum"]["usd"])
